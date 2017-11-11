af6d360c37a4459cc5c4b81e9371c237d9721583
Khalil Horton <horton.k223@gmail.com>
2017-11-10 18:16:00 -0600

    Added timer when Pneumatic system is full.

--- src/org/usfirst/frc/team3596/robot/subsystems/Pnewmatics.java
+++ src/org/usfirst/frc/team3596/robot/subsystems/Pnewmatics.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team3596.robot.subsystems;
 import org.usfirst.frc.team3596.robot.Robot;
 
 import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -10,11 +11,15 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class Pnewmatics extends Subsystem {
 	Compressor compressor;
+	Timer timer;
+	
 
 
 	public Pnewmatics() {
 		if (Robot.isReal()) {
 			compressor = new Compressor(50);
+			timer = new Timer();
+			
 		}
 	}
 
@@ -23,6 +28,7 @@ public class Pnewmatics extends Subsystem {
 
 	public void start() {
 		if (compressor.getPressureSwitchValue()) {
+			Timer.delay(30);
 			compressor.start();
 		}
 		else
